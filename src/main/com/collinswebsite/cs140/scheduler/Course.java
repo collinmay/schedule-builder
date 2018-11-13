71bd76485b63317261233591df65b8cc28873879
Collin May <CMay6574@student.whatcom.edu>
2018-11-12 19:52:46 -0800

    move source to src/main

--- src/main/com/collinswebsite/cs140/scheduler/Course.java
+++ src/main/com/collinswebsite/cs140/scheduler/Course.java
@@ -29,7 +29,7 @@ public class Course {
      * both become "MATH&163".
      */
     public static String normalizeCourseId(String id) {
-        return id.replaceAll(" ", "");
+        return id.replaceAll(" ", "").toUpperCase();
     }
 
     /**
